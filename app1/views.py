aad1aaa832ebfa15b778beae25d0773e2a6255e2
UmaBharathiMuniyappa <umahsk06@gmail.com>
2023-11-09 18:55:25 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Uma(request):
+    return render(request,'uma.html')
+def Kiko(request):
+    return render(request,'kiko.html')
+def Chinni(request):
+    return render(request,'chinni.html')
+def Nalli(request):
+    return render(request,'nalli.html')
